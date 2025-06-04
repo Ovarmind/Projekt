fd5ee58702491b91747bb13410c050f26ce95e8b
Ovarmind <121432101+Ovarmind@users.noreply.github.com>
2025-06-04 19:54:03 +0200

    Add files via upload

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,5 @@
 export const config = {
     port: process.env.PORT || 3100,
     supportedDevicesNum: 17,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://BartekDanek:haslo1@iot.l6cyw99.mongodb.net/IoT?retryWrites=true&w=majority',
-    JwtSecret: "secret"
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://BartekDanek:haslo@iot.l6cyw99.mongodb.net/IoT?retryWrites=true&w=majority'
 };
